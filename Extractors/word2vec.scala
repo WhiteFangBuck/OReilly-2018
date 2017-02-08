bcb15eaf30a0b25d819541e1704ac02fb7c8e0de
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2017-02-08 11:03:39 -0800

    Create word2vec.scala

---
+++ Extractors/word2vec.scala
@@ -0,0 +1,36 @@
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
+
+object word2vec {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("Word2VecExample")
+    val sc = new SparkContext(conf)
+
+    // $example on$
+    val input = sc.textFile("data/mllib/sample_lda_data.txt").map(line => line.split(" ").toSeq)
+
+    val word2vec = new Word2Vec()
+
+    val model = word2vec.fit(input)
+
+    val synonyms = model.findSynonyms("1", 5)
+
+    for((synonym, cosineSimilarity) <- synonyms) {
+      println(s"$synonym $cosineSimilarity")
+    }
+
+    // Save and load model
+    model.save(sc, "myModelPath")
+    val sameModel = Word2VecModel.load(sc, "myModelPath")
+    // $example off$
+
+    sc.stop()
+  }
+}
+
+// scalastyle:on println
