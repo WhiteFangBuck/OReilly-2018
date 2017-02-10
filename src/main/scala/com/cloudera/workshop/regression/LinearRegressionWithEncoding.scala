38de074e813815fbbd03c72d402299d7736bd740
vsingh <vsingh@cloudera.com>
2017-02-10 06:36:16 -0800

    Added package information

--- src/main/scala/com/cloudera/workshop/regression/LinearRegressionWithEncoding.scala
+++ src/main/scala/com/cloudera/workshop/regression/LinearRegressionWithEncoding.scala
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.strata.workshop
+package org.cloudera.workshop
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
