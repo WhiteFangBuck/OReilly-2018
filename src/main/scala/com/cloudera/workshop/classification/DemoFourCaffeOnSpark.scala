14b9347546c4ec160946e1d035e1a577562dc382
vsingh <vsingh@cloudera.com>
2017-03-03 19:56:08 -0800

    Adding the solutions and exercises for a few clustering

---
+++ src/main/scala/com/cloudera/workshop/classification/DemoFourCaffeOnSpark.scala
@@ -0,0 +1,35 @@
+package com.cloudera.workshop.classification
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by vsingh on 3/3/17.
+  */
+object DemoFourCaffeOnSpark {
+
+
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    /**
+      * Read the input data
+      */
+    var input = "data/Housing.csv"
+    if (args.length > 0) {
+      input = args(0)
+    }
+
+    val spark = SparkSession
+      .builder
+      .appName("DemoFourCaffeOnSpark")
+      .master("local")
+      .getOrCreate()
+
+    spark.stop()
+
+  }
+
+}
