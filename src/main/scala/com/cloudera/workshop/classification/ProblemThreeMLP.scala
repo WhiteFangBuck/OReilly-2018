7ec26f35b2cfce61a08ffbb20394bd6408756769
vsingh <vsingh@cloudera.com>
2017-03-03 19:06:53 -0800

    Adding the solutions and exercises for a few regression

---
+++ src/main/scala/com/cloudera/workshop/classification/ProblemThreeMLP.scala
@@ -0,0 +1,33 @@
+package com.cloudera.workshop.classification
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by vsingh on 3/3/17.
+  */
+object ProblemThreeMLP {
+
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+
+    /**
+      * Read the input data
+      */
+    var input = "data/Housing.csv"
+    if (args.length > 0) {
+      input = args(0)
+    }
+
+    val spark = SparkSession
+      .builder
+      .appName("ProblemTwoGradientBoostedTree")
+      .master("local")
+      .getOrCreate()
+
+    spark.stop()
+
+  }
+}
