5c5b019815b03c3a69fbd661a0dcf9fa3e02eff7
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2017-03-11 22:06:27 -0800

    Create SnowballStemmer.java

---
+++ src/main/java/com/cloudera/workshop/SnowballStemmer.java
@@ -0,0 +1,7 @@
+package com.cloudera.workshop;
+
+import java.lang.reflect.InvocationTargetException;
+
+public abstract class SnowballStemmer extends SnowballProgram {
+    public abstract boolean stem();
+};
