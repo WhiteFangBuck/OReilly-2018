600b8ed84f378a4f9e515d353f007903fd39c39d
WhiteFangBuck <WhiteFangBuck@users.noreply.github.com>
2017-05-21 20:49:25 +0100

    Create tokenizersExample.scala

---
+++ scala-scripts/workshop/solutions/transformers/tokenizersExample.scala
@@ -0,0 +1,32 @@
+{{
+import org.apache.spark.ml.attribute.Attribute
+import org.apache.spark.ml.feature._
+import org.apache.spark.sql.functions.{col, udf}
+
+/**
+  * Tokenizer
+  */
+val data = Seq(
+      (0, " It was a bright cold day in April, and the clocks were striking thirteen."),
+      (1, "The sky above the port was the color of television, tuned to a dead channel."),
+      (2, "It was love at first sight.")
+    )
+
+val sentenceDataFrame = spark.createDataFrame(data).toDF("id", "sentence")
+
+val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
+val regexTokenizer = new RegexTokenizer()
+      .setInputCol("sentence")
+      .setOutputCol("words")
+      .setPattern("\\W") // alternatively .setPattern("\\w+").setGaps(false)
+
+val countTokens = udf { (words: Seq[String]) => words.length }
+
+val tokenized = tokenizer.transform(sentenceDataFrame)
+    tokenized.select("sentence", "words")
+      .withColumn("tokens", countTokens(col("words"))).show(false)
+ 
+ val regexTokenized = regexTokenizer.transform(sentenceDataFrame)
+    regexTokenized.select("sentence", "words")
+      .withColumn("tokens", countTokens(col("words"))).show(false)
+}}
