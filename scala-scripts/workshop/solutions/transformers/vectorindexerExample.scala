e2e9cf966194f7c0790f08539f83d61d79fec80a
Jeffrey Shmain <jshmain@gmail.com>
2017-09-21 15:20:17 -0400

    Update vectorindexerExample.scala

--- scala-scripts/workshop/solutions/transformers/vectorindexerExample.scala
+++ scala-scripts/workshop/solutions/transformers/vectorindexerExample.scala
@@ -1,4 +1,4 @@
-{{
+
 import org.apache.spark.ml.attribute.Attribute
 import org.apache.spark.ml.feature._
 import org.apache.spark.sql.functions.{col, udf}
@@ -6,12 +6,12 @@ import org.apache.spark.sql.functions.{col, udf}
 /**
   * Combine above using VectorIndexer
   */
-val libSVMData = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
+val libSVMData = spark.read.format("libsvm").load("/data/mllib/sample_libsvm_data.txt")
 
-val vIndexer = new VectorIndexer()
-      .setInputCol("features")
-      .setOutputCol("indexed")
-      .setMaxCategories(10)
+val vIndexer = new VectorIndexer().
+      setInputCol("features").
+      setOutputCol("indexed").
+      setMaxCategories(10)
 
 val indexerModel = vIndexer.fit(libSVMData)
 
@@ -22,5 +22,3 @@ val categoricalFeatures: Set[Int] = indexerModel.categoryMaps.keys.toSet
 // Create new column "indexed" with categorical values transformed to indices
 val indexedData = indexerModel.transform(libSVMData)
     indexedData.show()
-    
-    }}
