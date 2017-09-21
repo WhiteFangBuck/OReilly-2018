46b6c42f44717b467e6c86cb444bf43392612449
Jeffrey Shmain <jshmain@gmail.com>
2017-09-21 13:45:56 -0400

    Update ProblemTwoLDA.scala

--- scala-scripts/workshop/solutions/clustering/ProblemTwoLDA.scala
+++ scala-scripts/workshop/solutions/clustering/ProblemTwoLDA.scala
@@ -32,8 +32,8 @@ val vocabSize: Int = 10000
   * Find a way to read all the files and attach an id to the files read.
   */
 
-val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-import sqlContext.implicits._
+//val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+//import sqlContext.implicits._
 
 val rawTextRDD = sc.wholeTextFiles(inputDir).map(_._2)
 val docDF = rawTextRDD.
