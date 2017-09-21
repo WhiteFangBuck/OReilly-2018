c98ac0cc49aeb71b8d9cc0b6a76ee47b833486b9
Jeffrey Shmain <jshmain@gmail.com>
2017-09-21 16:12:00 -0400

    Update ProblemTwoGradientBoostedTree.scala

--- scala-scripts/workshop/exercises/regression/ProblemTwoGradientBoostedTree.scala
+++ scala-scripts/workshop/exercises/regression/ProblemTwoGradientBoostedTree.scala
@@ -6,10 +6,6 @@ import org.apache.spark.sql.SparkSession
 /**
   * Created by vsingh on 3/3/17.
   */
-object ProblemTwoGradientBoostedTree {
-
-
-  def main(args: Array[String]) {
 
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
@@ -17,18 +13,5 @@ object ProblemTwoGradientBoostedTree {
     /**
       * Read the input data
       */
-    var input = "data/Housing.csv"
-    if (args.length > 0) {
-      input = args(0)
-    }
+    var input = "/data/Housing.csv"
 
-    val spark = SparkSession
-      .builder
-      .appName("ProblemTwoGradientBoostedTree")
-      .master("local")
-      .getOrCreate()
-
-    spark.stop()
-
-  }
-}
