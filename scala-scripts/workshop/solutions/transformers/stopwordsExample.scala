f1ea395b581a2719366be8bae7b219fa5dd6dfaf
Jeffrey Shmain <jshmain@gmail.com>
2017-09-21 15:15:35 -0400

    Update stopwordsExample.scala

--- scala-scripts/workshop/solutions/transformers/stopwordsExample.scala
+++ scala-scripts/workshop/solutions/transformers/stopwordsExample.scala
@@ -1,4 +1,3 @@
-{{
 import org.apache.spark.ml.attribute.Attribute
 import org.apache.spark.ml.feature._
 import org.apache.spark.sql.functions.{col, udf}
@@ -15,32 +14,31 @@ val data = Seq(
 val sentenceDataFrame = spark.createDataFrame(data).toDF("id", "sentence")
 
 val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
-val regexTokenizer = new RegexTokenizer()
-      .setInputCol("sentence")
-      .setOutputCol("words")
-      .setPattern("\\W") // alternatively .setPattern("\\w+").setGaps(false)
+val regexTokenizer = new RegexTokenizer().
+      setInputCol("sentence").
+      setOutputCol("words").
+      setPattern("\\W") // alternatively .setPattern("\\w+").setGaps(false)
 
 val countTokens = udf { (words: Seq[String]) => words.length }
 
 val tokenized = tokenizer.transform(sentenceDataFrame)
-    tokenized.select("sentence", "words")
-      .withColumn("tokens", countTokens(col("words"))).show(false)
+    tokenized.select("sentence", "words").
+      withColumn("tokens", countTokens(col("words"))).show(false)
 
 val regexTokenized = regexTokenizer.transform(sentenceDataFrame)
-    regexTokenized.select("sentence", "words")
-      .withColumn("tokens", countTokens(col("words"))).show(false)
+    regexTokenized.select("sentence", "words").
+      withColumn("tokens", countTokens(col("words"))).show(false)
  
  /**
   * StopWords Removal Example
   */
-var stopWordFile = "data/stopwords.txt"
+var stopWordFile = "/data/stopwords.txt"
 val stopwords = spark.sparkContext.textFile(stopWordFile).collect
-val remover = new StopWordsRemover()
-      .setStopWords(stopwords)
-      .setCaseSensitive(false)
-      .setInputCol("words")
-      .setOutputCol("filtered")
+val remover = new StopWordsRemover().
+      setStopWords(stopwords).
+      setCaseSensitive(false).
+      setInputCol("words").
+      setOutputCol("filtered")
 
 remover.transform(regexTokenized).show(false)
       
-}}
