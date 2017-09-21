ade73480a5cba5389552c6d3ad4f07896af8af43
Jeffrey Shmain <jshmain@gmail.com>
2017-09-21 15:09:38 -0400

    Update word2vecExample.scala

--- scala-scripts/workshop/solutions/extractors/word2vecExample.scala
+++ scala-scripts/workshop/solutions/extractors/word2vecExample.scala
@@ -1,6 +1,5 @@
 import org.apache.spark.ml.feature._
 
-{{
     // Input data: Each row is a bag of words from a sentence or document.
     val sentenceData = spark.createDataFrame(Seq(
       (0.0, "It was a bright cold day in April, and the clocks were striking thirteen."),
@@ -12,13 +11,12 @@ import org.apache.spark.ml.feature._
     val wordsData = tokenizer.transform(sentenceData)
 
     // Learn a mapping from words to Vectors.
-    val word2Vec = new Word2Vec()
-      .setInputCol("words")
-      .setOutputCol("result")
-      .setVectorSize(3)
-      .setMinCount(0)
+    val word2Vec = new Word2Vec().
+      setInputCol("words").
+      setOutputCol("result").
+      setVectorSize(3).
+      setMinCount(0)
     val model = word2Vec.fit(wordsData)
 
     val result = model.transform(wordsData)
     result.show(false)  
-}}
